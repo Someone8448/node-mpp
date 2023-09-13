406da05dd5aead41b8ae2d15c9568edfeccfb9c1
Someone8448 <96267197+Someone8448@users.noreply.github.com>
2023-09-13 13:09:11 -0700

    Create db.js

---
+++ db.js
@@ -0,0 +1,17 @@
+module.exports = (file, defaults) => {
+    var data = {}
+    data.data = null
+    data.read = () => {
+        try {
+        data.data = JSON.parse(require('fs').readFileSync(file,'utf8'))
+        } catch (error) {
+            data.data = (defaults === undefined) ? {} : defaults
+        }
+    }
+    data.write = () => {
+        require('fs').writeFileSync(file,JSON.stringify(data.data))
+    }
+    data.read();
+    data.write();
+    return data
+}
